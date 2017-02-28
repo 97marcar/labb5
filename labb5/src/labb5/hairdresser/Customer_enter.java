ea3879f94eb4d97224fd0e3dbc38804a28a13b9a
Slashy27 <oscar.ferm1993@gmail.com>
2017-02-28 15:40:38 +0100

    custome enter

--- labb5/src/labb5/hairdresser/Customer_enter.java
+++ labb5/src/labb5/hairdresser/Customer_enter.java
@@ -3,5 +3,37 @@ package labb5.hairdresser;
 import labb5.simulator.Event;
 
 public class Customer_enter implements Event{
+	final int dressers = 5;
+	final int waitchairs = 15;
+	private FIFO f;
 	
+	public int getDressers() {
+		return dressers;
+		
+	}
+	public int getWaitChairs() {
+		return waitchairs;
+		
+	}
+	public boolean isFull() {
+		if(f.size() > waitchairs) {
+			return true;
+		}
+		return false;
+		
+	}
+	public void addToQueue(Object item) {
+		f.add(item);
+	}
+	public void removeFromQueue() {
+		f.removeFirst();
+		
+	}
+	public void addFrontLine() {
+		
+		
+	}
+	public void removeLast() {
+		
+	}
 }
