6923ee301438ba6a8c0c3e8426c55ee4f4f4e94c
97marcar <marcus.carlsson97@gmail.com>
2017-02-28 15:40:49 +0100

    close

--- labb5/src/labb5/hairdresser/HairSaloonClose.java
+++ labb5/src/labb5/hairdresser/HairSaloonClose.java
@@ -3,5 +3,13 @@ package labb5.hairdresser;
 import labb5.simulator.Event;
 
 public class HairSaloonClose implements Event{
-
+	private boolean isClosed = false;
+	
+	public void changeClosedState(){
+		isClosed = !isClosed;
+	}
+	
+	public boolean getClosedState(){
+		return isClosed;
+	}
 }
