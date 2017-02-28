c0d17b31658ed408e03a7c44a0e1c965dcaf9ef1
97marcar <marcus.carlsson97@gmail.com>
2017-02-28 15:33:01 +0100

    Customer

--- labb5/src/labb5/hairdresser/Customer.java
+++ labb5/src/labb5/hairdresser/Customer.java
@@ -1,5 +1,38 @@
 package labb5.hairdresser;
-
+/**
+ * 
+ * @author Marcus, Oscar, Henrik
+ *
+ */
 public class Customer {
-
+	private boolean isSatisfied = true;
+	private int id;
+	
+	/** 
+	 * @param id unique customer id
+	 */
+	public Customer(int id){
+		this.id = id;
+	}
+	
+	/**
+	 * Change state of customers satisfaction
+	 */
+	public void changeSatisfaction(){
+		isSatisfied = !isSatisfied;
+	}
+	
+	/**
+	 * @return state of customers satisfaction
+	 */
+	public boolean getSatisfaction(){
+		return isSatisfied;
+	}
+	
+	/**
+	 * @return id of the customer
+	 */
+	public int getId(){
+		return id;
+	}
 }
