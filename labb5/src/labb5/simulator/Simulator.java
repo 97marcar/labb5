87b309bb9118c02f5866fbad779601d0836a3ce3
Slashy27 <oscar.ferm1993@gmail.com>
2017-02-28 16:44:41 +0100

    random randy simulator

--- labb5/src/labb5/simulator/Simulator.java
+++ labb5/src/labb5/simulator/Simulator.java
@@ -1,5 +1,23 @@
 package labb5.simulator;
 
+import java.util.Random;
+
+import labb5.hairdresser.Customer;
+
 public class Simulator {
+	Random randy = new Random();
+	Customer casty;
+	
+	public void setSatisfaction() {
+		if(casty.getSatisfaction() == true) {
+			if((randy.nextInt(100) + 1) <= 20) {
+			casty.changeSatisfaction();
+			}
+		}else if(casty.getSatisfaction() == false) {
+			if((randy.nextInt(100) + 1) <= 80) {
+				casty.changeSatisfaction();
+			}
+		}
+	}
 
 }
