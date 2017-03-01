807de31490d8203a9be5b28ebe0a1480e9286210
Henrik Möller <henmll-4@student.ltu.se>
2017-03-01 10:40:03 +0100

    Ändringar i simllator, Kod bör lyftas ut och läggas in i Event

--- labb5/src/labb5/simulator/Simulator.java
+++ labb5/src/labb5/simulator/Simulator.java
@@ -5,19 +5,23 @@ import java.util.Random;
 import labb5.hairdresser.Customer;
 
 public class Simulator {
-	Random randy = new Random();
-	Customer casty;
-	
-	public void setSatisfaction() {
-		if(casty.getSatisfaction() == true) {
-			if((randy.nextInt(100) + 1) <= 20) {
-			casty.changeSatisfaction();
-			}
-		}else if(casty.getSatisfaction() == false) {
-			if((randy.nextInt(100) + 1) <= 80) {
-				casty.changeSatisfaction();
-			}
-		}
-	}
 
 }
+
+
+// Bör lyftas in i ett event. 
+/** Random randy = new Random();
+Customer casty;
+
+public void setSatisfaction() {
+	if(casty.getSatisfaction() == true) {
+		if((randy.nextInt(100) + 1) <= 20) {
+		casty.changeSatisfaction();
+		}
+	}else if(casty.getSatisfaction() == false) {
+		if((randy.nextInt(100) + 1) <= 80) {
+			casty.changeSatisfaction();
+		}
+	}
+}
+*/
