4eeb4fb385e8a4b43684ddcc5b744c1834538545
Henrik Möller <henmll-4@student.ltu.se>
2017-03-03 10:10:15 +0100

    Sortering funktion för event enl tid i kön klar.

--- labb5/src/labb5/simulator/EventQueue.java
+++ labb5/src/labb5/simulator/EventQueue.java
@@ -3,13 +3,29 @@ package labb5.simulator;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class EventQueue {
 	
 	private List<Event> eventQ = new ArrayList<Event>();
 	
 	public void add(Event event){
 		eventQ.add(event);
+		sort();
 	}
+	
+	public void sort() {
+		int n = eventQ.size();
+		for (int j = 1; j < n; j++) {
+			Event k = eventQ.get(j);
+			int i = j - 1;
+			while (i >= 0 && eventQ.get(j).getTime()>k.getTime()) {
+				eventQ.add(i+1, eventQ.get(i));
+				i--;
+			}
+			eventQ.add(i+1,k);
+		}
+	}
+	
 	
 	public void removeFirst(){
 		eventQ.remove(0);
