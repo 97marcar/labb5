5771a55944a4e9681a363acdf605fde4c512d9ad
Henrik Möller <henmll-4@student.ltu.se>
2017-03-04 15:53:54 +0100

    Kompletterat med javadoc

--- labb5/src/labb5/simulator/EventQueue.java
+++ labb5/src/labb5/simulator/EventQueue.java
@@ -9,14 +9,20 @@ public class EventQueue {
 	
 	private List<Event> eventQ = new ArrayList<Event>();
 	
+	/**
+	 * Add event to event queue and sort the queue rising 
+	 * depending on the time which the event is supposed to happen.
+	 * @param event
+	 */
 	public void add(Event event){
 		eventQ.add(event);
 		sort();
 	}
 	
-	
-	//Byggd på metoden Insertsort enl HJ IF14
-	public void sort() {
+	/**
+	 * Help method to sort the list rising depending on time. 
+	 */
+	private void sort() {
 		int n = eventQ.size();
 		for (int j = 1; j < n; j++) {
 			Event k = eventQ.get(j);
@@ -29,22 +35,37 @@ public class EventQueue {
 		}
 	}
 	
-	
+	/**
+	 * Remove first event in queue
+	 */
 	public void removeFirst(){
 		eventQ.remove(0);
 	}
+	/**
+	 * Clear the whole queue
+	 */
 	public void clear() {
 		eventQ.clear();
 	}
+
+	/**
+	 * @return size of the queue
+	 */
 	public int size() {
 		return eventQ.size();
 	}
 	
-	
+	/**
+	 * @return true if queue is empty
+	 */
 	public boolean isEmpty(){
 		return(eventQ.isEmpty());
 	}
 	
+	/**
+	 * @return first element in queue if no element exist
+	 * exception is thrown 
+	 */
 	public Event getFirst(){
 		if(eventQ.isEmpty()) {
 			throw new NoSuchElementException();
