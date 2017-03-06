1bec44b35166f172e2380aee847795005a02b392
Slashy27 <oscar.ferm1993@gmail.com>
2017-03-06 10:53:25 +0100

    mainstart

--- labb5/src/labb5/HairSaloonMain.java
+++ labb5/src/labb5/HairSaloonMain.java
@@ -1,5 +1,18 @@
 package labb5;
 
+import labb5.hairdresser.SaloonState;
+import labb5.hairdresser.SaloonView;
+import labb5.simulator.EventQueue;
+import labb5.simulator.Simulator;
+
 public class HairSaloonMain {
+	
+	public void main (String[] args) {
+		SaloonState state = new SaloonState();
+		EventQueue queue = new EventQueue();
+		Simulator sim = new Simulator();
+		SaloonView view = new SaloonView(state, queue);
+		
+	}
 
 }
