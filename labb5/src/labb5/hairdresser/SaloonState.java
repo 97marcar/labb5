d9b20e8a699b32721464d77f318d46bb73bbcd39
97marcar <marcus.carlsson97@gmail.com>
2017-03-06 09:08:12 +0100

    saker

--- labb5/src/labb5/hairdresser/SaloonState.java
+++ labb5/src/labb5/hairdresser/SaloonState.java
@@ -6,8 +6,8 @@ import labb5.simulator.Event;
 import labb5.simulator.State;
 
 public class SaloonState extends State{
-	final int dressers = 5;
-	final int waitchairs = 15;
+	private final int DRESSERS = 5;
+	private final int WAITCHAIRS = 15;
 	private FIFO f;
 	private int customerCounter = 0;
 	private HaircutReady hr;
@@ -15,13 +15,17 @@ public class SaloonState extends State{
 	private Event currentEvent;
 	private double hmin=1, hmax=2;
 	private double dmin=1, dmax=2;
-	long seed=1116;
-	double lambda=1.2;
+	private long seed=1116;
+	private double lambda=1.2;
+	private ExponentialRandomStream timeNewCustomer;
+	private UniformRandomStream timeHairCut;
+	private UniformRandomStream timeDissatisfiedReturn;
+	private int numberOfUnsatified;
 	
 	public SaloonState(){
-		ExponentialRandomStream timeNewCustomer = new ExponentialRandomStream (lambda, seed);
-		UniformRandomStream timeHairCut = new UniformRandomStream(hmin,hmax,seed);
-		UniformRandomStream timeDissatisfiedReturn = new UniformRandomStream(dmin,dmax,seed);
+		timeNewCustomer = new ExponentialRandomStream (lambda, seed);
+		timeHairCut = new UniformRandomStream(hmin,hmax,seed);
+		timeDissatisfiedReturn = new UniformRandomStream(dmin,dmax,seed);
 		// Chansen att bli missnöjd hanteras just nu i HairCutReady, bör man flytta vissar delar därifrån?
 	}
 	
@@ -51,19 +55,13 @@ public class SaloonState extends State{
 	}
 	
 	public boolean lineFull(){
-		if(f.size() >= waitchairs){
+		if(f.size() >= WAITCHAIRS){
 			return true;
 		}
 		return false;
 	}
 	public int getTotalCustomer() {
 		return customerCounter;
-	}
-	public int getDissatisfiedCustomer() {
-		return hr.diss();
-	}
-	public int getReformed(){
-		return hr.reformed();
 	}
 	
 	public Customer createCustomer(){
@@ -72,28 +70,64 @@ public class SaloonState extends State{
 		return c;
 	}
 	
-	public void createCustomer_enter(int time){
-		Customer_enter event = new Customer_enter(this, createCustomer(), time);
+	public void createCustomer_enter(){
+		Customer_enter event = new Customer_enter(this, createCustomer(), timeNewCustomer.next(), timeHairCut.next());
 		//Här ska den på något sätt läggas in i kön.
 	}
 	
-	public void createHairCutReady(Customer c, int time){
-		HaircutReady event = new HaircutReady();//Konstruktor ej färdig i klassen
+	public void createHairCutReady(Customer c, double time){
+		HaircutReady event = new HaircutReady(this, c, seed, time, timeDissatisfiedReturn.next());
 		//Här ska den på något sätt läggas in i kön.
 	}
 	
-	public void createDissatisfiedReturn(Customer c, int time ){
-		DissatisfiedReturn event = new DissatisfiedReturn(c, time);
+	public void createDissatisfiedReturn(Customer c, double time ){
+		DissatisfiedReturn event = new DissatisfiedReturn(c, time, timeHairCut.next());
 		//Här ska den på något sätt läggas in i kön.
 	}
 	//Kanske en metod för att skicka in ett event i eventkön?
 	//FUNKAR INTE :( ska användas i view.
-//	public void setCurrentEvent(Event currentEvent) {
-//		this.currentEvent = currentEvent;
-//	}
-//	public Event getCurrentEvent(Event event) {
-//		return currentEvent;
-//		
-//	}
+	public void setCurrentEvent(Event currentEvent) {
+		this.currentEvent = currentEvent;	
+	}
+	public Event getCurrentEvent(Event event) {
+		return currentEvent;
+		
+	}
+	
+	public void addUnsatisfied(){
+		numberOfUnsatified++;
+	}
+	
+	public int getWaitChairs(){
+		return WAITCHAIRS;
+	}
+	
+	public int getDressers(){
+		return DRESSERS;
+	}
+	
+	public double getLambda(){
+		return lambda;
+	}
+	
+	public double gethmax(){
+		return hmax;
+	}
+	
+	public double gethmin(){
+		return hmin;
+	}
+	
+	public double getdmax(){
+		return dmax;
+	}
+	
+	public double getdmin(){
+		return dmin;
+	}
+	
+	public long getSeed(){
+		return seed;
+	}
 		
 	}
