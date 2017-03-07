139975ed06c9f4aca5b996135f10ea46c7109f6e
Slashy27 <oscar.ferm1993@gmail.com>
2017-03-07 09:26:39 +0100

    view

--- labb5/src/labb5/hairdresser/SaloonView.java
+++ labb5/src/labb5/hairdresser/SaloonView.java
@@ -1,32 +1,32 @@
 package labb5.hairdresser;
+import labb5.hairdresser.SaloonState;
 import labb5.simulator.EventQueue;
 import java.util.Observable;
 
 import labb5.simulator.View;
 
 public class SaloonView extends View{
+	private HaircutReady hr;
 	
 	private SaloonState state;
-	private Customer_enter enter;
-	private FIFO f;
-	private HaircutReady ready;
-	private Start start;
 	private EventQueue q;
 	
 	public SaloonView(SaloonState state, EventQueue q) {
+		this.state = state;
+		this.q = q;
 		saloonInitialLayout();
 	}
 	
 	public void saloonInitialLayout() {
-		System.out.println("Closing time of the day......" + state.getClosingTime());
-		System.out.println("Total number of chairs" + state.getDressers());
-		System.out.println("Maximum queue size" + state.getWaitChairs());
-		System.out.println("Lambda (Customer/timeunit entering" + state.getLambda());
-		System.out.println("hmin and hmax (cutting time interval" + "[" + state.gethmin() + "," + state.gethmax() + "]");
-		System.out.println("dmin and dmax (return time interval" + "[" + state.getdmin() + "," + state.getdmax() + "]");
-		System.out.println("Risk dissatisfied returns" + ready.getFailProcent() + "%");
-		System.out.println("Seed used in pseudo random generator" + state.getSeed());
-		System.out.println("-------------------------------------");
+		System.out.println("Closing time of the day = " + state.getClosingTime());
+		System.out.println("Total number of chairs = " + state.getDressers());
+		System.out.println("Maximum queue size = " + state.getWaitChairs());
+		System.out.println("Lambda (Customer/timeunit entering = " + state.getLambda());
+		System.out.println("hmin and hmax (cutting time interval = " + "[" + state.gethmin() + "," + state.gethmax() + "]");
+		System.out.println("dmin and dmax (return time interval = " + "[" + state.getdmin() + "," + state.getdmax() + "]");
+//		System.out.println("Risk dissatisfied returns" + hr.getFailProcent() + "%");
+		System.out.println("Seed used in pseudo random generator = " + state.getSeed());
+		System.out.println("-------------------------------------------");
 		System.out.println("- Time" + "\t" + "Event" +"\t" + " Id" + 
 		"\t" + "Idle" +"\t" + " TIdle" + "\t" + "TWait" +"\t" +
 				"InQ" + "\t" + "Cut" +"\t" + " Lost" + "\t" + "Ret " +"-" );	
